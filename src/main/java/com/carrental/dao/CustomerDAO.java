7387cb56daabd98d57fd4fcee80bfe385689dd4b
NavSethi2006 <navinsethi2006@gmail.com>
2026-02-15 19:28:26 -0500

    removed bin for merge conflicts

--- src/main/java/com/carrental/dao/CustomerDAO.java
+++ src/main/java/com/carrental/dao/CustomerDAO.java
@@ -1,5 +1,6 @@
 package main.java.com.carrental.dao;
 
+import java.security.SecureRandom;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -10,17 +11,19 @@ public class CustomerDAO {
 	public CustomerDAO() {
 		
 	}
+	
+	
 
-	public Customer findByUsername(String username) {
+	public static Customer findByUsername(String username) {
 		String query = "SELECT * FROM customers WHERE name = '"+username+"'";
 		ResultSet result = MySQL.fetch(query);
 		Customer customer = null;
 		try {
 			String id = result.getString(0);
-			// skipped 1 since index 1 is the name
+			String name = result.getString(1);
 			String email = result.getString(2);
 			String password = result.getString(3);
-			customer = new Customer(id, username, email, password);
+			customer = new Customer(id, name, email,password);
 
 		} catch(SQLException e) {
 			System.err.print("This email doesent exist in our database, would you like to register?");
@@ -28,6 +31,27 @@ public class CustomerDAO {
 		
 		
 		return customer;
+	}
+	
+	public static Customer findByID (String customer_id) {
+		String query = "SELECT * FROM customers WHERE id = '"+customer_id+"'";
+		ResultSet result = MySQL.fetch(query);
+		Customer customer = null;
+		try {
+			String id = result.getString(0);
+			String name = result.getString(1);
+			String email = result.getString(2);
+			String password = result.getString(3);
+			
+			customer = new Customer(id, name, email, password);
+
+		} catch(SQLException e) {
+			System.err.print("This email doesent exist in our database, would you like to register?");
+		}
+		
+		
+		return customer;
+		
 	}
 	
 	public static Customer findByEmail(String email) {
@@ -39,25 +63,23 @@ public class CustomerDAO {
 			String name = result.getString(1);
 			// skipped 2 since index 2 is email
 			String password = result.getString(3);
+			
 			customer = new Customer(id, name, email, password);
 
 		} catch(SQLException e) {
 			System.err.print("This email doesent exist in our database, would you like to register?");
 		}
+		
+		
 		return customer;
+		
 	}
 	
 
 	
 	public static void insertRecord(String username, String email, String password) {
 		String query = "INSERT INTO customers(name, email) VALUES('"+username+"','"+email+"'"+password+"');";
-		boolean insertion = MySQL.insert(query);
-		
-		if(insertion) {
-			// return frontend success code
-		} else {
-			System.err.print("Error with registeration, please try again later");
-		}
+		MySQL.insert(query);
 	}
 	
 	
